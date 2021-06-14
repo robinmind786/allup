fd6d2cf7af3da996079655d034d2224494f7b9ff
Robin Mind <43693130+robinmind786@users.noreply.github.com>
2021-06-14 13:58:49 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+var acc = document.getElementsByClassName("openCat");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = document.getElementById('myDropdown');
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    } 
+  });
+}
+
+
